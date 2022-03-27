67e20680222e403d2fc33f15f33b87ba0fdbb4f3
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-03-27 22:18:09 +0300

    Add date_posted to Image model

--- gallery/models.py
+++ gallery/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length = 50)
 
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
@@ -13,6 +19,7 @@ class Image(models.Model):
     description = models.CharField(max_length = 255)
     location = models.ForeignKey(Location, on_delete = models.CASCADE)
     category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
         return self.name
