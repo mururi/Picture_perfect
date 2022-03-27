e9887fe1bb9c5cf4f22096ec835493f5bad513ac
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-03-27 20:12:34 +0300

    Add first template

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,7 @@
+from urllib import response
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Welcome to Picture Perfect')
+    return render(request, 'index.html', {})
