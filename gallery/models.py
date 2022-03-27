1d2b926d2aa5429053d5be26642297aa2499d82d
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-03-27 22:10:56 +0300

    Add models and configure image upload

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,19 @@
+from unicodedata import category
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length = 50)
+
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length = 60)
+    description = models.CharField(max_length = 255)
+    location = models.ForeignKey(Location, on_delete = models.CASCADE)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
