7df18de2f694b5bae89fc16f2c2fde9d3cde3a7e
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-03-29 00:36:07 +0300

    Update home page to get all images in database

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,9 @@
 from urllib import response
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Image
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html', {})
+    images = Image.get_all_images()
+    return render(request, 'index.html', {"images": images})
